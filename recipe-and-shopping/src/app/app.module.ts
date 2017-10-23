ae851970a5839802fee6d98c6f4e4e03c92a06cc
Kocann <kocela.ann@gmail.com>
2017-10-23 19:29:45 +0200

    Adding all components files

--- recipe-and-shopping/src/app/app.module.ts
+++ recipe-and-shopping/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './ShoppingList/ShoppingList/shopping-list.component';
+import { ShoppingListEditComponent } from './ShoppingList/ShoppingListEdit/shopping-list-edit.component';
+import { IngredientComponent } from './ingredient/ingredient.component'
+import { RecipeComponent } from './RecipeBook/Recipe/recipe.component';
+import { RecipeDetailComponent } from './RecipeBook/RecipeDetail/recipe-detail.component';
+import { RecipeItemComponent } from './RecipeBook/RecipeItem/recipe-item.component';
+import { RecipeListComponent } from './RecipeBook/RecipeList/recipe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    IngredientComponent,
+    RecipeComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule

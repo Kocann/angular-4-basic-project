30066d58f6a277705b871240ea62411d6bc52e7b
Kocann <kocela.ann@gmail.com>
2017-11-22 19:58:46 +0100

    Adding simpler solution

--- recipe-and-shopping/src/app/shared/dropdown.directive.ts
+++ recipe-and-shopping/src/app/shared/dropdown.directive.ts
@@ -13,16 +13,23 @@ import {
 // works!! but deleted from html becuase bootstrap works! :)
 
 export class DropdownDirective {
-  @HostBinding('class.show') show: boolean = true;
+  @HostBinding('class.show') isOpen: boolean = true;
 
   constructor(private elRef: ElementRef, private rendered: Renderer2) {}
 
   @HostListener('click') click() {
-    if (this.show) {
+    if (this.isOpen) {
       this.rendered.addClass(this.elRef.nativeElement, 'show');
     } else {
       this.rendered.removeClass(this.elRef.nativeElement, 'show');
     }
-    this.show = !this.show;
+    this.isOpen = !this.isOpen;
   }
+
+  //much simpler it would be like so:
+  // @HostBinding('class.show') isOpen: boolean = false;
+  // @HostListener('click') toggleOpen() {
+  //   this.isOpen = !this.isOpen;
+  // }
+  // because everytime is open is set to true the show class will be added to the host element
 }
